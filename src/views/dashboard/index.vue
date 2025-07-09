ba9b63a15ee5d0ea4617c6ff36f6aee47cd7a1bc
Jianlei Qu <lyu_qujianlei@163.com>
2025-07-09 08:44:17 +0800

    环境变量测试

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,6 +2,9 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
     <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <el-divider />
+    <div class="dashboard-text">env: {{ env }}</div>
+    <div class="dashboard-text">url: {{ url }}</div>
   </div>
 </template>
 
@@ -10,6 +13,12 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      env: process.env.NODE_ENV,
+      url: process.env.VUE_APP_BASE_API
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
